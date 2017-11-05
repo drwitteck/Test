43db2dafa56d56cdfde5a8748a0eaa2c517214bd
Derek Witteck <drwitteck@gmail.com>
2017-11-05 14:56:31 -0500

    Test

--- app/src/main/java/com/theshulmonies/lookowlt/MainActivity.java
+++ app/src/main/java/com/theshulmonies/lookowlt/MainActivity.java
@@ -10,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void skrrPop(){
+        int quickMath = 2 + 2;
+    }
 }

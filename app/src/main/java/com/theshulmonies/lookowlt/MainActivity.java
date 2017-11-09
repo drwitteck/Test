8bec32c28c9895b2a91022c9b3ef78b5e8b0860f
drwitteck <drwitteck@gmail.com>
2017-11-08 22:52:01 -0500

    Created ReportsFeedActivity with objects, variables, and methods listed on UML.

--- app/src/main/java/com/theshulmonies/lookowlt/MainActivity.java
+++ app/src/main/java/com/theshulmonies/lookowlt/MainActivity.java
@@ -10,9 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    //Activity to hold post feed
-    public void userPostFeedActivity(){
-
-    }
 }

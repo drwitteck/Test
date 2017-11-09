2af097b7eeb164801a389033ca65701201c8d4ed
daveseverns <david.w.severns@gmail.com>
2017-11-08 19:52:43 -0500

    created test class for order objects

---
+++ app/src/test/java/com/theshulmonies/lookowlt/OrderObjectUnitTest.java
@@ -0,0 +1,16 @@
+package com.theshulmonies.lookowlt;
+
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ * Created by davidseverns on 11/8/17.
+ */
+
+public class OrderObjectUnitTest {
+    public OrderObjectUnitTest() {
+        super();
+    }
+}

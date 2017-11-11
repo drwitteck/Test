cf01d08c82d23382688643a5e2e4aec41458d3a4
Philip <pcappelli901@gmail.com>
2017-11-10 23:25:40 -0500

    Added TDD test code to verify the user has a @temple.edu email address

--- app/src/main/java/com/theshulmonies/lookowlt/Utilities/Utility.java
+++ app/src/main/java/com/theshulmonies/lookowlt/Utilities/Utility.java
@@ -9,15 +9,10 @@ import java.util.regex.Pattern;
 
 public class Utility {
 
-
-
     public boolean isValidEmail(String emailAddress) {
-        final String TEMPLE_DOMAIN = "@temple.edu";
         final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@*@temple\\.edu";
-
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         Matcher matcher = pattern.matcher(emailAddress);
         return matcher.matches();
-
     }
 }

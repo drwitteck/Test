600a03f5d7dfe1c8b993346eed8d98cbc871a2d4
Derek Witteck <drwitteck@gmail.com>
2017-11-11 18:18:39 -0500

    Testing testBranch. FYI - Needed to go to Update Project under VCS settings to get the branch to show as an active branch that I could checkout

--- app/src/main/java/com/theshulmonies/lookowlt/MainActivity.java
+++ app/src/main/java/com/theshulmonies/lookowlt/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
         //test for derek
         //test for dave
         //test for .gitignore
+        //test for testBranch from derek
     }
 }

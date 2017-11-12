407e361d2e03984385e1c320c8087480ae195b27
daveseverns <david.w.severns@gmail.com>
2017-11-12 16:47:31 -0500

    special report test added

--- app/src/test/java/com/theshulmonies/lookowlt/ReportObjectUnitTest.java
+++ app/src/test/java/com/theshulmonies/lookowlt/ReportObjectUnitTest.java
@@ -38,6 +38,7 @@ public class ReportObjectUnitTest {
 
     @Test
     public void shouldSetSpecialReportTrue(){
+        mEventsReport.sendSpecialReport();
         assertTrue(mEventsReport.isSpecialReport());
     }
 

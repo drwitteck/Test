9298b8f3c4eb8f55fa38423ecda7d3869253e2df
Philip <pcappelli901@gmail.com>
2017-11-12 15:00:23 -0500

    commit before merging to TestBranch - changed ReportsFeedActivity to MainActivity to allow for no merge conflicts

--- app/src/main/java/com/theshulmonies/lookowlt/MainActivity.java
+++ app/src/main/java/com/theshulmonies/lookowlt/MainActivity.java
@@ -4,14 +4,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class ReportsFeedActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -26,7 +22,7 @@ public class ReportsFeedActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if (firebaseAuth.getCurrentUser() == null) {
-                    Intent loginIntent = new Intent(ReportsFeedActivity.this, LoginActivity.class);
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
                     loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(loginIntent);
                 }

5dc23ff76a9d78eb999bf5640352e25c01021d65
Derek Witteck <drwitteck@gmail.com>
2017-11-12 14:28:31 -0500

    Added auth code to ReportsFeedActivity for log in capability

--- app/src/main/java/com/theshulmonies/lookowlt/ReportsFeedActivity.java
+++ app/src/main/java/com/theshulmonies/lookowlt/ReportsFeedActivity.java
@@ -1,23 +1,57 @@
 package com.theshulmonies.lookowlt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
+//import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ReportsFeedActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     //ReportsFeedAdapter feedAdapter;
     //FirebaseUtility feedFbUtility;
     private RecyclerView mRecyclerView;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null) {
+                    Intent loginIntent = new Intent(ReportsFeedActivity.this, LoginActivity.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(loginIntent);
+                }
+            }
+        };
+
+        /*
+        tempSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+            }
+        });
+        */
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     public void onReportItemClicked(){
